dcaf3a0bbd6d828c868acae0f5870e3ee9824b76
Garik <garik.kalash@gmail.com>
2020-05-07 18:51:35 +0400

    asd

--- src/main/java/com/gk/portfolio/PortfolioApplication.java
+++ src/main/java/com/gk/portfolio/PortfolioApplication.java
@@ -2,12 +2,19 @@ package com.gk.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PortfolioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
     }
 
+    @GetMapping
+    public String f() {
+        return "aa";
+    }
 }
