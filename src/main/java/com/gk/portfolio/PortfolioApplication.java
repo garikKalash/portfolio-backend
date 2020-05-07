72ffb9fa98cb5bc643251902317b4c4830cd30cf
Garik <garik.kalash@gmail.com>
2020-05-07 19:56:05 +0400

    finish

--- src/main/java/com/gk/portfolio/PortfolioApplication.java
+++ src/main/java/com/gk/portfolio/PortfolioApplication.java
@@ -2,19 +2,14 @@ package com.gk.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Profile;
 
+@Profile("prod")
 @SpringBootApplication
-@RestController
 public class PortfolioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
     }
 
-    @GetMapping
-    public String f() {
-        return "aa";
-    }
 }
