8d0ab865e894ab48031801d295b924a2a781996d
Garik <garik.kalash@gmail.com>
2020-05-07 21:12:14 +0400

    insert users

---
+++ src/main/resources/prod/db/migration/V1_0_2__insert_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO public."user" (id, name, surname, password, role, email, phone, address, skype) VALUES (1, 'Garik', 'Kalashyan', '$2a$10$zte6uauMbTdEsxcqGr5NQ.zg11f7WoYnzHd0MdEu0bCgezayZAc0i', 'me', 'garik.kalash@gmail.com', '+37494477522', 'Armenia, Yerevan', 'garikkalash999');
+INSERT INTO public."user" (id, name, surname, password, role, email, phone, address, skype) VALUES (2, 'hr', 'hr', '$2a$10$sq2xIrFgcRsaRZSlrjciUuwTsRKUJOGOSfJT03iTg3vtiI5AvmirS', 'hr', 'hr@hr.com', 'hr', 'hr', 'hr');
+INSERT INTO public."user" (id, name, surname, password, role, email, phone, address, skype) VALUES (3, 'tech_guy', 'tech_guy', '$2a$10$b7qWxuECNKxM7aLkMwUCAeKgcwAFYB/R7yne/3i06.yN50zS3J1vG', 'technical_guy', 'tech@tech.com', 'tech', 'tech', 'tech');
+
